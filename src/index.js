e88fa5a2f0d731f7c156137cafb77bc123e487ac
Alicja <dul.alicja@gmail.com>
2017-08-17 19:37:11 +0200

    Adding Youtube API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
+const API_KEY ='AIzaSyBqVassz9mv2yhVvHIKyXWg8Q2UVNXJvf0	'
+
 const App = () => {
-  return <div>HI</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+ );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
