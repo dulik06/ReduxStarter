47bc603bacc4f4170f739d322a16f6182973cfd1
Alicja <dul.alicja@gmail.com>
2017-08-18 17:47:04 +0200

    Adding search API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
-const API_KEY ='AIzaSyBqVassz9mv2yhVvHIKyXWg8Q2UVNXJvf0	'
+const API_KEY ='AIzaSyAXabSPOk4LnbtGaXYt4nKQuQBp-oZ1KEI'
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
- );
+YTSearch({
+  key: API_KEY,
+  term: 'cats'
+},
+  function(data) { console.log(data)}
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+   );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
