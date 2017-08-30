fc314f1dc7747f0393a0dd224633059b6fd6a31a
Alicja <dul.alicja@gmail.com>
2017-08-30 20:03:43 +0200

    Adding functionality

--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/vidoe_detail'
 
 const API_KEY ='AIzaSyAXabSPOk4LnbtGaXYt4nKQuQBp-oZ1KEI'
 
-YTSearch({
-  key: API_KEY,
-  term: 'cats'
-},
-  function(data) { console.log(data)}
-);
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [], selectedVideo: null };
+    YTSearch({ key: API_KEY, term: 'cats' }, (videos) =>
+    { this.setState({ videos: videos, selectedVideo: videos[0] });
+  });
+  }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos = {this.state.videos} />
       </div>
    );
   }
