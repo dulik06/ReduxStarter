9027d692d74f1c6b89c2fc26a9cec48db48c32ee
Alicja <dul.alicja@gmail.com>
2017-08-31 19:09:13 +0200

    Search bar functionality

--- src/index.js
+++ src/index.js
@@ -14,14 +14,22 @@ class App extends Component {
 
     this.state = { videos: [], selectedVideo: null };
 
-    YTSearch({ key: API_KEY, term: 'cats' }, (videos) =>
-    { this.setState({ videos: videos, selectedVideo: videos[0] });
+    this.videoSearch('javascript')
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) =>
+    { this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
   });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
         videos = {this.state.videos}
